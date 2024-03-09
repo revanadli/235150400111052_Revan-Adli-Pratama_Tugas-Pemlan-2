bd8adc8a887bb2bedffb86d20806d97eb24f1932
revanadli <152585691+revanadli@users.noreply.github.com>
2024-03-09 17:44:22 +0700

    mahasiswa.java

---
+++ mahasiswa.java
@@ -0,0 +1,32 @@
+public class mahasiswa {
+
+    private String Nama;
+    private String NIM;
+    private String Alamat;
+
+
+    public String getNama(){
+        return Nama;
+    }
+
+    public void setNama(String nama){
+        this.Nama = nama;
+    }
+
+    public String getNIM(){
+        return NIM;
+    }
+
+    public void setNIM(String nim){
+        this.NIM = nim;
+    }
+
+    public String getAlamat() {
+        return Alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.Alamat = alamat;
+    }
+}
+
